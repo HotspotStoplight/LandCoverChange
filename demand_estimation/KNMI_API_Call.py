45e4308e78e1662ae04693111975f2cde36a1bd8
olivegardener <74479442+olivegardener@users.noreply.github.com>
2024-03-18 13:50:16 -0400

    OA_push

---
+++ demand_estimation/KNMI_API_Call.py
@@ -0,0 +1,3 @@
+curl --location --request GET \
+    "https://api.dataplatform.knmi.nl/open-data/v1/datasets/Actuele10mindataKNMIstations/versions/2/files" \
+    --header "Authorization: <eyJvcmciOiI1ZTU1NGUxOTI3NGE5NjAwMDEyYTNlYjEiLCJpZCI6ImE1OGI5NGZmMDY5NDRhZDNhZjFkMDBmNDBmNTQyNjBkIiwiaCI6Im11cm11cjEyOCJ9>"
